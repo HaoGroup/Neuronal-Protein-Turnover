c5fcb95767964fc3aad9d847fa1f2978f1c361bd
Medhasweta <msen22@gwu.edu>
2023-04-17 11:01:32 -0400

    applying to the whole dataset

--- Visualization/src/initialgraphs.py
+++ Visualization/src/initialgraphs.py
@@ -8,8 +8,8 @@ import numpy as np
 # %%
 
 # df = pd.read_excel("/Users/medhaswetasen/Documents/GitHub/Neuronal-Protein-Turnover/Visualization/iMN_Peptide_Dataset.xlsx")
-# df = pd.read_excel("/Users/medhaswetasen/Documents/GitHub/Neuronal-Protein-Turnover/Visualization/data/iMN_Peptide_Dataset.xlsx")
-df = pd.read_excel("../data/iMN_Peptide_Dataset.xlsx") # use relative paths, non-user-specific
+df = pd.read_excel("/Users/medhaswetasen/Documents/GitHub/Neuronal-Protein-Turnover/Visualization/data/iMN_Peptide_Dataset.xlsx")
+# df = pd.read_excel("../data/iMN_Peptide_Dataset.xlsx") # use relative paths, non-user-specific
 # print(df.head().to_string())
 # %%
 df_1 = df[["PG.Genes","Peptide","iMN_Day0_Light_Relative_Abundance","iMN_Day1_Light_Relative_Abundance","iMN_Day2_Light_Relative_Abundance","iMN_Day4_Light_Relative_Abundance","iMN_Day6_Light_Relative_Abundance"]]
@@ -114,6 +114,24 @@ for unique in nonna:
     plt.show()
 
 
+
+
+for unique in unique_entries:
+    df_3 = df_1[df_1["PG.Genes"] == unique ]
+    df_4 = df_2[df_2["PG.Genes"] == unique ]
+    pep = df_3["Peptide"].unique()
+    df_melted_4 = pd.melt(df_4, id_vars=['PG.Genes', 'Peptide'], value_vars=[0, 1, 2, 4, 6], var_name='Values', value_name='Heavy Medium')
+    f_melted_sorted_4 = df_melted_4.sort_values(by=['Peptide', 'Values'])
+    f_melted_sorted_4 = f_melted_sorted_4.set_index("Values")
+    f_melted_sorted_4 = f_melted_sorted_4[["Heavy Medium"]]
+    df_melted_3 = pd.melt(df_3, id_vars=['PG.Genes', 'Peptide'], value_vars=[0, 1, 2, 4, 6], var_name='Values', value_name='Light Medium')
+    f_melted_sorted_3 = df_melted_3.sort_values(by=['Peptide', 'Values'])
+    f_melted_sorted_3 = f_melted_sorted_3.set_index("Values")
+    merged_df = pd.merge(f_melted_sorted_3, f_melted_sorted_4, left_index=True, right_index=True)
+    for p in pep:
+        df_33 = merged_df[merged_df["Peptide"] == p]
+        df_33.plot(y=['Heavy Medium', 'Light Medium'])
+    plt.show()
 
 
 
